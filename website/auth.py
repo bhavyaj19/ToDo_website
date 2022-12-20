8567a71b1c7f3d7211f9cc52586a03d9e4c7f475
bhavyaj19 <bhavyaj525@gmail.com>
2022-12-20 16:56:34 +0530

    adding route in auth and other minor stuff

--- website/auth.py
+++ website/auth.py
@@ -1,3 +1,15 @@
 from flask import Blueprint
 
 auth = Blueprint('auth',__name__)
+
+@auth.route('/login')
+def login():
+    return '<h1>LOGIN</h1>'
+
+@auth.route('/logout')
+def logout():
+    return '<h1>LOGOUT</h1>'
+
+@auth.route('/signup')
+def signup():
+    return '<h1>SIGNUP</h1>'
